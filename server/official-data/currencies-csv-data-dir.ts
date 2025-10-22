1ec896e6fa0b46ac0e16d3ee198e20590e408cdc
Alessandro Amella <44754837+alessandroamella@users.noreply.github.com>
2025-10-22 02:59:10 +0200

    fix csv path

--- server/official-data/currencies-csv-data-dir.ts
+++ server/official-data/currencies-csv-data-dir.ts
@@ -1,6 +1,9 @@
 import { join } from 'node:path';
+import { envs } from 'server/config/envs';
 
 export const CURRENCIES_CSV_DATA_DIR = join(
   import.meta.dirname,
-  '../../currency_data',
+  `${envs.NODE_ENV === 'production' ? '' : '../'}../currency_data`,
 );
+
+console.log(`Currencies CSV data directory set to: ${CURRENCIES_CSV_DATA_DIR}`);
