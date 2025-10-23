619dd3f5e3ffbac789e4530216cba08d5e31f12a
Alessandro Amella <44754837+alessandroamella@users.noreply.github.com>
2025-10-24 01:16:52 +0200

    fix public folder, update manifest

--- server/vite.ts
+++ server/vite.ts
@@ -6,7 +6,6 @@ import express, { type Express } from 'express';
 import { nanoid } from 'nanoid';
 import { createLogger, createServer as createViteServer } from 'vite';
 import viteConfig from '../vite.config';
-import { envs } from './config/envs';
 
 const viteLogger = createLogger();
 
@@ -70,10 +69,7 @@ export async function setupVite(app: Express, server: Server) {
 }
 
 export function serveStatic(app: Express) {
-  const distPath = path.resolve(
-    import.meta.dirname,
-    envs.NODE_ENV === 'production' ? 'public' : '../public',
-  );
+  const distPath = path.resolve(import.meta.dirname, '../public');
 
   if (!existsSync(distPath)) {
     throw new Error(
@@ -81,6 +77,7 @@ export function serveStatic(app: Express) {
     );
   }
 
+  console.log(`Serving static files from: ${distPath}`);
   app.use(express.static(distPath));
 
   // fall through to index.html if the file doesn't exist
