6fdb27fb1cdcc792f82a82385414722ce3991b79
Alessandro Amella <44754837+alessandroamella@users.noreply.github.com>
2025-11-03 16:06:22 +0100

    fix root path

--- server/paths/index.ts
+++ server/paths/index.ts
@@ -1,8 +1,12 @@
 import { join } from 'node:path';
+import { envs } from 'server/config/envs';
 import { logger } from 'server/utils/logger';
 
 // for some strange reason, before I had to do this hacky way to get the root path
 // `${envs.NODE_ENV === 'production' ? '' : '../'}../`,
-export const ROOT_PATH = join(import.meta.dirname, '../../');
+export const ROOT_PATH = join(
+  import.meta.dirname,
+  envs.NODE_ENV === 'development' ? '../../' : '../',
+);
 
 logger.info(`Project root path resolved to: ${ROOT_PATH}`);
