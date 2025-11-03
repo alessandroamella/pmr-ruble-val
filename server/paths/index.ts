93716d61ae30f35037df02f524c96402679a48af
Alessandro Amella <44754837+alessandroamella@users.noreply.github.com>
2025-11-03 16:05:24 +0100

    print root path

--- server/paths/index.ts
+++ server/paths/index.ts
@@ -1,5 +1,8 @@
 import { join } from 'node:path';
+import { logger } from 'server/utils/logger';
 
 // for some strange reason, before I had to do this hacky way to get the root path
 // `${envs.NODE_ENV === 'production' ? '' : '../'}../`,
 export const ROOT_PATH = join(import.meta.dirname, '../../');
+
+logger.info(`Project root path resolved to: ${ROOT_PATH}`);
